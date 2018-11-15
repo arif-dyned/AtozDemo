889250268c3024a4b02f38b31e544047c6fd46c3
Ahmad Arif Saputra <ganggaputra46@gmail.com>
2018-11-15 10:48:05 +0700

    add config assets

---
+++ config/assets.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Assets
+|--------------------------------------------------------------------------
+| Define registered assets folder so you can easyly call css or js by dynamic
+|
+|
+*/
+
+return [
+    'css' => [
+        'style' => 'css/style.css',
+        'app' => 'app.css'
+    ],
+    'js' => []
+];
